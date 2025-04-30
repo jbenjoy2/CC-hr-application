b5498b1109942d74d5eeb2014edde965063f7236
Jason Benjoya <45188358+jbenjoy2@users.noreply.github.com>
2025-04-30 08:05:25 -0400

    initialize frontend and update backend routing logic

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,12 +3,13 @@ import employeesRouter from "./routes/employees";
 import { errorHandler } from "./middleware/errorHandler";
 
 const app = express();
-
+const apiRouter = express.Router();
 // Body parser
 app.use(express.json());
-
 // Mount employees router
-app.use("/employees", employeesRouter);
+apiRouter.use("/employees", employeesRouter);
+
+app.use("/api", apiRouter);
 
 // Global error handler (always last)
 app.use(errorHandler);
