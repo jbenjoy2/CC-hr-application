da839b9415d3b637ee6a56db7744a56e99b4556f
Jason Benjoya <45188358+jbenjoy2@users.noreply.github.com>
2025-05-01 12:24:55 -0400

    fix error on frontend render

--- frontend/src/pages/employee-list/index.tsx
+++ frontend/src/pages/employee-list/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "./styles.css";
 import { useEmployees } from "../../hooks/useEmployees";
 import EmployeesTable from "./components/employees-table";
